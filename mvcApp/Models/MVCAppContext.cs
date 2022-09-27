0902f8b57ab84eb3deb01e03d4e1f721b04f8e8d
LENOVO <LENOVO@LAPTOP-D4JI72RI>
2022-09-27 21:07:14 +0200

    Add project files.

---
+++ mvcApp/Models/MVCAppContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace mvcApp.Models
+{
+    public class MVCAppContext: DbContext
+    {
+        public MVCAppContext(DbContextOptions<MVCAppContext> options): base(options)
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ mvcApp/Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace mvcApp.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
